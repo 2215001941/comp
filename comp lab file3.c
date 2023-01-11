c2d413bf3661f4c0a5de8255439ce6774f9f5ba5
Vidhi Khandelwal <122196946+2215001941@users.noreply.github.com>
2023-01-11 18:26:25 +0530

    Create comp lab file3.c

---
+++ comp lab file3.c
@@ -0,0 +1,17 @@
+//Write a c program to count frequency of each element in a given array.//
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n],f[n];
+	for(int i=0;i<n;i++){
+	scanf("%d",&a[i]);
+	f[i]==-1;}
+	for(int i=0;i<n;i++){
+		int c=0;
+		for(int j=0;j<n;j++)
+	printf("%d occurs %d times",f,c);
+	return 0;
+}
+}
