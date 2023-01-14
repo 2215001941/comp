16bfbe2b700a84434d8a6a1cc7a0df04d971c1d5
Vidhi Khandelwal <122196946+2215001941@users.noreply.github.com>
2023-01-14 20:40:22 +0530

    Create comp lab file13.c

---
+++ comp lab file13.c
@@ -0,0 +1,17 @@
+//Write a c program to print a given pattern.//
+#include<stdio.h>
+int main()
+{
+   int n;
+   printf("Enter the number of rows: ");
+   scanf("%d",&n);
+   for(int r=1; r<=n; r++)
+   {
+     for(int c=1; c<=n; c++)
+     {
+       printf("* ");
+     }
+     printf("\n");
+   }
+   return 0;
+}
